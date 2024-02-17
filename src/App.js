dc194eea1232b2e6697aa9d5c0768e1160c39e49
JackEH2112 <jacke_hughes@hotmail.co.uk>
2024-02-17 19:37:55 +0000

    Upload missed changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Codecademy! <br/>
+          I am hacking
         </p>
         <a
           className="App-link"
